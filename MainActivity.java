51bba39fdf012dbbfb8dd8721a251d2ee96bddbf
Ritika Gupta <91077713+RG0102@users.noreply.github.com>
2023-12-12 20:06:28 +0000

    Update MainActivity.java

--- MainActivity.java
+++ MainActivity.java
@@ -18,6 +18,11 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+//I have commented on some part code below because I am using Room Database. If you uncomment my code and run my program. It crashes my program. The app won't even start?
+// This is the error I am getting in the LogCat:  android.content.ActivityNotFoundException: Unable to find explicit activity class {com.example.foodorderapp2/com.example.foodorderapp2.FoodOrderActivity}; have you declared this activity in your AndroidManifest.xml, or does your intent not match its declared <intent-filter>?
+
+        
+
         //Access the Room Database instance from MyApp
 //        MyApp myApp = (MyApp) getApplication();
 //        FoodOrderDatabase appDatabase = myApp.getAppDatabase();
